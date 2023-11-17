53c0b909effa5c84e9bcc3c81fc0ff3d89d7cf47
ALIJON <ALI>
2023-11-17 12:02:31 -0500

    File Share resources added

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Create First Resource Group 1
+# Create First Resource Group 
 module "rg1" {
   source   = "../Module/Resource_Group"
   name     = var.rg1_name
@@ -27,7 +27,8 @@ module "snet" {
 }
 
 # Create a Piblic IP
-resource "azurerm_public_ip" "pip" {
+module "pip" {
+  source              = "../Module/Public_ip"
   name                = var.pip_name
   resource_group_name = module.rg1.name
   location            = module.rg1.location
@@ -36,10 +37,14 @@ resource "azurerm_public_ip" "pip" {
 
 # Create a network interface card fro our VM to use
 module "nic" {
-  source              = "../Module/Network_Security_Group"
-  name                = var.nic_name
-  resource_group_name = module.rg1.name
-  location            = module.rg1.location
+  source                        = "../Module/Network_Interface_Card"
+  name                          = var.nic_name
+  resource_group_name           = module.rg1.name
+  location                      = module.rg1.location
+  ip_config_name                = var.nic_ip_name
+  subnet_id                     = module.snet.subnet_id
+  private_ip_address_allocation = var.nic_private_ip_allocation
+  public_ip_address_id          = module.pip.public_ip_id
 }
 
 # Create a Windows VM and connect it to our Network Interface Card
@@ -51,17 +56,100 @@ module "vm" {
   size                  = var.vm_size
   admin_username        = var.vm_username
   admin_password        = var.vm_password
-  network_interface_ids = module.nic.nsg_id
+  network_interface_ids = [module.nic.nic_id]
 
   # OS Disk
   caching              = var.vm_caching
   storage_account_type = var.vm_stg_acc_type
 
   # Source image reference
-  publisher = var.vm_publisher
-  offer     = var.vm_offer
-  sku       = var.vm_sku
-  vm_version=var.vm_version
- 
+  publisher  = var.vm_publisher
+  offer      = var.vm_offer
+  sku        = var.vm_sku
+  vm_version = var.vm_version
+
 }
 
+# Create Second Resource Group 
+module "rg2" {
+  source   = "../Module/Resource_Group"
+  name     = var.rg2_name
+  location = var.rg2_location
+}
+
+# Create Storage Account
+module "stor_acc" {
+  source                   = "../Module/Storage_Account"
+  stor_name                = var.stor_name
+  resource_group_name      = module.rg2.name
+  location                 = module.rg2.location
+  account_tier             = var.stor_acc_tier
+  account_replication_type = var.stor_acc_rep_type
+  # Networking Rules
+  default_action = var.stor_default_action
+  ip_rules       = var.stor_ip_rules
+}
+# # Create container
+module "container1" {
+  source                = "../Module/Storage_Container"
+  name                  = var.container_name
+  storage_account_name  = module.stor_acc.name
+  container_access_type = var.container_access_type
+}
+
+# Create SAS token
+module "storage_sas_token" {
+  source            = "../Module/Storage_sas_token"
+  connection_string = module.stor_acc.sas
+  https_only        = var.stor_sas_https_only
+  signed_version    = var.stor_sas_signed_version
+  # Resource types
+  service   = var.stor_sas_service
+  container = var.stor_sas_container
+  object    = var.stor_sas_object
+  # Services
+  blob  = var.stor_sas_blob
+  queue = var.stor_sas_queue
+  table = var.stor_sas_table
+  file  = var.stor_sas_file
+  # Token Timer
+  start  = var.stor_sas_start
+  expiry = var.stor_sas_expiry
+
+  # Permissions
+  read    = var.stor_sas_read
+  write   = var.stor_sas_write
+  delete  = var.stor_sas_delete
+  list    = var.stor_sas_list
+  add     = var.stor_sas_add
+  create  = var.stor_sas_create
+  update  = var.stor_sas_update
+  process = var.stor_sas_process
+  tag     = var.stor_sas_tag
+  filter  = var.stor_sas_filter
+}
+
+# Role Assignment
+data "azurerm_subscription" "primary" {}
+
+data "azurerm_client_config" "example" {}
+
+module "role_assignment" {
+  source = "../Module/Role_Assignment"
+
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = var.role_definition_name
+  principal_id         = data.azurerm_client_config.example.object_id
+}
+
+# Create File share
+module "file_share" {
+  source                 = "../Module/File_Share"
+  file_share_name        = var.file_share_name
+  storage_account_name   = module.stor_acc.name
+  file_share_quota       = var.file_share_quota
+  file_share_id          = var.file_share_id
+  file_share_permissions = var.file_share_permissions
+  file_share_start       = var.file_share_start
+  file_share_expiry      = var.file_share_expiry
+}
--- variables.tf
+++ variables.tf
@@ -47,6 +47,14 @@ variable "nic_name" {
   type = string
 }
 
+variable "nic_ip_name" {
+  type = string
+}
+
+variable "nic_private_ip_allocation" {
+  type = string
+}
+
 # Virtual Machine
 variable "vm_name" {
   type = string
@@ -90,5 +98,141 @@ variable "vm_version" {
   type = string
 }
 
+# Resource Group 2
+variable "rg2_name" {
+  type = string
 
+}
+variable "rg2_location" {
+  type = string
+}
 
+# Storage account
+variable "stor_name" {
+  type = string
+}
+
+variable "stor_acc_tier" {
+  type = string
+}
+
+variable "stor_acc_rep_type" {
+  type = string
+}
+
+variable "stor_default_action" {
+  type = string
+}
+
+variable "stor_ip_rules" {
+  type = list(string)
+}
+# Storage container
+variable "container_name" {
+  type = string
+}
+variable "container_access_type" {
+  type = string
+}
+
+# Storage SAS Token
+variable "stor_sas_https_only" {
+  type = bool
+}
+variable "stor_sas_signed_version" {
+  type = string
+}
+# Resource Types
+variable "stor_sas_service" {
+  type = bool
+}
+variable "stor_sas_container" {
+  type = bool
+}
+variable "stor_sas_object" {
+  type = bool
+}
+# Services
+variable "stor_sas_blob" {
+  type = bool
+}
+variable "stor_sas_queue" {
+  type = bool
+}
+variable "stor_sas_table" {
+  type = bool
+}
+variable "stor_sas_file" {
+  type = bool
+}
+# SAS Token Validity Period
+variable "stor_sas_start" {
+  type = string
+}
+variable "stor_sas_expiry" {
+  type = string
+}
+# Permissions
+variable "stor_sas_read" {
+  type = bool
+}
+variable "stor_sas_write" {
+  type = bool
+}
+variable "stor_sas_delete" {
+  type = bool
+}
+variable "stor_sas_list" {
+  type = bool
+}
+variable "stor_sas_add" {
+  type = bool
+}
+variable "stor_sas_create" {
+  type = bool
+}
+variable "stor_sas_update" {
+  type = bool
+}
+variable "stor_sas_process" {
+  type = bool
+}
+variable "stor_sas_tag" {
+  type = bool
+}
+variable "stor_sas_filter" {
+  type = bool
+}
+
+# Role Assignment
+variable "role_definition_name" {
+  type    = string
+  default = "Storage Blob Data Owner"
+}
+
+# File Share
+variable "file_share_name" {
+  type = string
+}
+
+variable "file_share_quota" {
+  type = string
+}
+
+# Id
+variable "file_share_id" {
+  type = string
+}
+
+# Access policy
+variable "file_share_permissions" {
+  type = string
+}
+
+variable "file_share_start" {
+  type = string
+}
+
+variable "file_share_expiry" {
+  type = string
+}
